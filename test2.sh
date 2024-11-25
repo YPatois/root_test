0bf179b068688c3f8a27004c86740ae6aa51a5c2
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2024-11-25 19:00:10 +0100

    Maybe

--- test2.sh
+++ test2.sh
@@ -1,4 +1,25 @@
-#!/bin/bash -x
+#!/bin/bash
+rm -rf build
+mkdir build
+
+cmake -B build
+cmake --build build
+root test.C -b -q
+
+
+mv TTestFit.h TTMTestFit.h
+
+sed -i 's/TTestFit.h/TTMTestFit.h/g' TTestFit.cxx
+
+rm -rf build
+mkdir build
+
+cmake -B build
+cmake --build build
+root test.C -b -q
+
+rm -f toto.h
+git restore TTestFit.h TTestFit.cxx
 
 mv TTestFit.h toto.h
 
@@ -9,9 +30,7 @@ mkdir build
 
 cmake -B build
 cmake --build build
-
-
 root test.C -b -q
 
 rm -f toto.h
-git restore TTestFit.h
+git restore TTestFit.h TTestFit.cxx
