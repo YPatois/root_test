ab52876ba65d318c0d521bba108dbb3da7ebe40a
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2024-12-05 13:58:53 +0100

    Could be lib/ or lib64/

--- test1.C
+++ test1.C
@@ -1,6 +1,7 @@
 // Run this code to include Thermus in your ROOT session
 R__ADD_INCLUDE_PATH(local/include/unTest)
 R__ADD_LIBRARY_PATH(local/lib64/unTest)
+R__ADD_LIBRARY_PATH(local/lib/unTest)
 R__LOAD_LIBRARY(unTest.so)
 
 void test1()
